de0c37d986a3a9011f802d6ed99e8588322f3368
Nurul Mukhlisa <numulaa@gmail.com>
2022-05-21 12:18:01 +0700

    make changes

--- 8-kyu/CW-2022-5-21/DNA.js
+++ 8-kyu/CW-2022-5-21/DNA.js
@@ -8,7 +8,7 @@
 
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
-let DNAtoRNA = (dna) => dna.replace(/T/g, "U");
+
 //without regex
 let DNAtoRNA = (dna) =>
   dna
