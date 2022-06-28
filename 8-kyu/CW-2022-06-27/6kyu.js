2cc22675809dfd4ad280c1924ab85d77f46935b7
Nurul Mukhlisa <numulaa@gmail.com>
2022-06-28 21:28:28 +0700

    new challenge

--- 8-kyu/CW-2022-06-27/6kyu.js
+++ 8-kyu/CW-2022-06-27/6kyu.js
@@ -29,18 +29,3 @@ const dataReverse = (data) => {
   return bytes;
 };
 
-//Mexican Wave
-//wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
-// function wave(str){
-//   // Code here
-//   let newStr= str.split(' ').join('')
-//   for(let i=0;i<newStr.length;i++){
-//     let newArr = new Array(newStr.length);
-//     return newArr.fill(newStr).map( (el, ind, arr) => el.replace(el.charAt(ind),el.charAt(ind).toUpperCase()));
-//   }
-// }
-
-const wave = (str) =>
-  [...str]
-    .map((s, i) => str.slice(0, i) + s.toUpperCase() + str.slice(i + 1))
-    .filter((x) => x != str);
