7a984fee8c853dff7b0d92da7621c1ecbb0d188c
Nurul Mukhlisa <numulaa@gmail.com>
2022-06-30 10:34:53 +0700

    new challenges

---
+++ 8-kyu/CW-2022-06-30/8kyu.js
@@ -0,0 +1,21 @@
+//altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// 'HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((a) => (a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()))
+    .join("");
+};
+
+//Filter out the geese
+
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((bird) => geese.indexOf(bird) < 0);
+}
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((bird) => !geese.includes(bird));
+}
