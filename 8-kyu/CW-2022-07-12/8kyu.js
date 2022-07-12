f129fbe70888b01d77af6bf80f5244180e11f253
Nurul Mukhlisa <numulaa@gmail.com>
2022-07-12 22:29:51 +0700

    new challenges

---
+++ 8-kyu/CW-2022-07-12/8kyu.js
@@ -0,0 +1,12 @@
+//Formatting decimal places #0
+//convert decimal to 2 decimal places
+function twoDecimalPlaces(n) {
+  // Your code here
+  return Number(n.toFixed(2));
+}
+
+//A Needle in the Haystack
+function findNeedle(haystack) {
+  // your code here
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
