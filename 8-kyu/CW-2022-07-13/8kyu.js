ab4421657c09589bb2ba73797d25a384059769e9
Nurul Mukhlisa <numulaa@gmail.com>
2022-07-13 22:35:07 +0700

    new challenges

---
+++ 8-kyu/CW-2022-07-13/8kyu.js
@@ -0,0 +1,11 @@
+//Drink about
+function peopleWithAgeDrink(old) {
+  return `drink ${
+    old < 14 ? "toddy" : old < 18 ? "coke" : old < 21 ? "beer" : "whisky"
+  }`;
+}
+
+//Grasshopper - Function syntax debugging
+function main(verb, noun) {
+  return verb + noun;
+}
