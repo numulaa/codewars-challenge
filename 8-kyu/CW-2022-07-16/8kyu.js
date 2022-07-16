d36a562d216ff5e72e8c9f2789e16f49bb04c6c1
Nurul Mukhlisa <numulaa@gmail.com>
2022-07-16 21:42:30 +0700

    new challenges

--- 8-kyu/CW-2022-07-16/8kyu.js
+++ 8-kyu/CW-2022-07-16/8kyu.js
@@ -1,58 +1,10 @@
-//Surface Area and Volume of a Box
-function getSize(width, height, depth) {
-  const surface = 2 * (width * height + height * depth + width * depth);
-  const volume = width * height * depth;
-  return [surface, volume];
-}
-//Thinkful - Number Drills: Pixelart planning
-function isDivisible(wallLength, pixelSize) {
-  //your code here
-  return wallLength % pixelSize === 0;
-}
-//repeatIt
-//"Hi", 2 --> "HiHi"
-//1234, 5 --> "Not a string"
-var repeatIt = function (str, n) {
-  let arr = [];
-  if (typeof str === "string") {
-    for (let i = 0; i < n; i++) {
-      arr.push(str);
-    }
-    return arr.join("");
-  } else {
-    return "Not a string";
-  }
-};
-//const repeatIt = (str,n) => typeof str == 'string' ? str.repeat(n) : 'Not a string'
+//return the last index of 1
+// given an array of 1 and 0's where the 1's are always behind the 0's (e.g [1100000] ...ect), return the index of the last 1. Return 0 if there are no 1's in the array
+// [1,1,0,0,0] -> 1
+// [0,0,0] -> 0
+// [] -> 0
+// [1,1,1,1,0,0,0] -> 3
 
-//Return the day
-function whatday(num) {
-  // put your code here
-  const days = {
-    1: "Sunday",
-    2: "Monday",
-    3: "Tuesday",
-    4: "Wednesday",
-    5: "Thursday",
-    6: "Friday",
-    7: "Saturday",
-  };
-  if (days.hasOwnProperty(num)) {
-    return days[num];
-  } else {
-    return "Wrong, please enter a number between 1 and 7";
-  }
-}
-//other way
-function whatday(num) {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  return days[num - 1] || "Wrong, please enter a number between 1 and 7";
+function returnTheLastIndex(arr) {
+  return arr.length !== 0 && arr.includes(1) ? arr.lastIndexOf(1) : 0;
 }
