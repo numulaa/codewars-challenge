5a22e874fc98792f150c52bf14dce374d34cfdd4
Nurul Mukhlisa <numulaa@gmail.com>
2022-07-27 22:05:13 +0700

    new challenge

--- 8-kyu/CW-2022-07-27/7kyu.js
+++ 8-kyu/CW-2022-07-27/7kyu.js
@@ -1,2 +1,14 @@
 //Reverse list
 const reverseList = (arr) => arr.reverse();
+
+//Divide and Conquer
+function divCon(x) {
+  const stringsNum = x
+    .filter((a) => typeof a === "string")
+    .map((el) => Number(el))
+    .reduce((a, c) => a + c, 0);
+  const numberNum = x
+    .filter((a) => typeof a === "number")
+    .reduce((a, c) => a + c, 0);
+  return numberNum - stringsNum;
+}
